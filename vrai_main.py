c9ee1e8ef74f750b11255c4ce770afa9953a3216
alex-jga <jaouena8@gmail.com>
2025-11-19 19:20:09 +0100

    Re Update vrai_main + bug à data (depot)

--- vrai_main.py
+++ vrai_main.py
@@ -1,4 +1,22 @@
+
+import json
+from data import *
+from tools import *
+clients = get_clients_base ()
+
+def ask_for_client_id (message):
+    client_id = input (message)
+    return client_id
+
+def client_is_valid (client_id):
+    print ("Liste des clés : ", list (clients.keys ()))
+    return client_id in list (clients.keys ())
+
+def client_is_not_valid (client_id):
+    return not (client_is_valid (client_id))
+
 def home_menu():
+    client_id = ask_and_validate_client_id ()
     print("\nWelcome to the ATM")
     print("1. Check Balance")
     print("2. Deposit Money")
@@ -8,8 +26,7 @@ def home_menu():
     return choice
 
 def run_atm():
-    pin = input("Please enter your PIN: ")
-    if pin == account['pin']:
+    
         while True:
             choice = display_menu()
             if choice == '1':
@@ -23,5 +40,5 @@ def run_atm():
                 break
             else:
                 print("Erreur. Veuillez rentrer à nouveau votre choix (1-4).")
-    else:
-        print("Erreur. Essayez à nouveau.")
+        else:
+            print("Erreur. Essayez à nouveau.")
